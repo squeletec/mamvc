0edbceb1b94cb47accba026daede28e1b831ffad
c0stra <ondrej.fischer@gmail.com>
2022-11-26 15:03:09 +0100

    Started working on handle.

--- mamvc-ui/mamvc-elements.js
+++ mamvc-ui/mamvc-elements.js
@@ -8,14 +8,25 @@ export function expander(model, enabled = boolean(true)) {
         .onClick(when(enabled, toggle(model)))
 }
 
-export function hbar(valueModel, className = 'progress') {
-    return div(className).position('absolute').bottom(0).left(0).height(100, '%').width(valueModel, '%')
+export function hbar(valueModel) {
+    return div('horizontal-bar').position('absolute').bottom(0).left(0).height(100, '%').width(valueModel, '%')
 }
 
-export function vbar(valueModel, className = 'progress') {
-    return div(className).position('absolute').bottom(0).left(0).width(100, '%').height(valueModel, '%')
+export function vbar(valueModel) {
+    return div('vertical-bar').position('absolute').bottom(0).left(0).width(100, '%').height(valueModel, '%')
 }
 
-export function progressBar(done, total, className = 'progress-bar') {
-    return div(className).backgroundSize(on(done, total).apply((d, t) => ((t > 0) ? 100 * d / t : 0)), '%')
+export function progressBar(done, total) {
+    return div('progress-bar').backgroundSize(on(done, total).apply((d, t) => ((t > 0) ? 100 * d / t : 0)), '%')
+}
+
+export function action(command) {
+    return a().setClass('action').onClick(command)
+}
+
+export function handle() {
+    return div('handle').onDrag(event => {
+        let dx = event.pageX - event.target.pageX
+        let dy = event.pageY - event.target.pageY
+    }).onDragend(event => {})
 }

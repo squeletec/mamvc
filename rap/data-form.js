571f8aba0539cd9e1968493995ab162b190d4615
c0stra <ondrej.fischer@gmail.com>
2022-12-15 09:32:33 +0100

    Added propper handling of checkbox

--- rap/data-form.js
+++ rap/data-form.js
@@ -1,15 +1,15 @@
 import {form, call, a, state} from "../trio.js";
 
 function merge(object, form) {
-    Object.entries(form.elements).filter(([k,v],i) => k !== '' + i).forEach(([k,v]) => object[k] = v.value)
+    Object.entries(form.elements).filter(([k,v],i) => k !== '' + i).forEach(([k,v]) => object[k] = v.type === 'checkbox' ? v.checked : v.value)
     return object
 }
 
-export function dataForm(restCall) {
+export function dataForm(restCall, mergeFunction = merge) {
     if(!restCall.data)
         restCall.setPostData(state({}), false)
     let postData = restCall.data
-    return form().onSubmit(event => postData.set(merge(postData.get(), event.target)))
+    return form().onSubmit(event => postData.set(mergeFunction(postData.get(), event.target)))
 }
 
 export function action(restCall) {

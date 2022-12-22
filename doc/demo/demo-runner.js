af7c4b005b568163088661e0841a7ae54d24138c
c0stra <ondrej.fischer@gmail.com>
2022-12-22 23:23:29 +0100

    Navigation

--- doc/demo/demo-runner.js
+++ doc/demo/demo-runner.js
@@ -1,16 +1,25 @@
 import { ul, li, byId, head, body, meta, a, h1, div, pre, boolean, toggle } from "../../trio.js"
 
-let examples = {
-    stateview: 'State view',
-    expander: 'Expander',
-    dialog: 'Dialog'
-}
+let examples = [
+    ['stateview', 'State view'],
+    ['expander', 'Expander'],
+    ['dialog', 'Dialog'],
+    ['data-table', 'Simple data table'],
+    ['search-table', 'Search data table'],
+    ['data-table-struct', 'Data table with nested structure'],
+    ['tree-table', 'Tree table'],
+    ['dynamic-tree-table', 'Tree table loading node children dynamically']
+]
+
+let positions = Object.fromEntries(examples.map(([v,k], i) => ['demo-' + v + '.html', i]))
 
 let displayList = boolean()
 
 function item(name, href) {
     return li().add(a('demo-' + href + '.html').add(name))
 }
+
+let p = positions[location.pathname.substring(location.pathname.lastIndexOf('/') + 1)]
 
 head().add(
     meta().name('viewport').content('width=device-width, initial-scale=1.0')
@@ -19,12 +28,14 @@ head().add(
 body().add(
     div('stripe').add('RAP Examples'),
     div('nav').add(
-        a('').add('< Prev'),
+        a(p > 0 ? examples[p - 1] : null).add('< Prev'),
         a('index.html').add('^ Up'),
         a().onClick(toggle(displayList)).add('...'),
-        a('').add('Next >')
+        a(p < examples.length - 1 ? examples[p + 1] : null).add('Next >')
     ),
     ul().display(displayList).setClass('list').add(
+        ...examples.map(([k,v]) => item(v, k))
+        /*
         item('State view', 'stateview'),
         item('Expander', 'expander'),
         item('Dialog', 'dialog'),
@@ -36,7 +47,7 @@ body().add(
         item('Layout with sidebar', 'layout-sidebar'),
         item("Drag'n'drop", 'drag-n-drop'),
         item("Post channel", "post-channel"),
-        item("Data form", "data-form")
+        item("Data form", "data-form")*/
     ),
     h1().add(' demo'),
     pre().id('demoSourceView').add(byId("demoSource").get().outerHTML)

db6479abbbde212f7cb80815d66b8fc504e51885
c0stra <ondrej.fischer@gmail.com>
2023-07-05 15:47:12 +0200

    Update commands.js

--- trio/controller/commands.js
+++ trio/controller/commands.js
@@ -1,4 +1,8 @@
 
 export function set(model, value) {
-    return () => model.set(value)
+    return isObservable(value) ? () => model.set(value.get()) : () => model.set(value)
+}
+
+export function toggle(model) {
+    return () => model.set(!model.get())
 }
